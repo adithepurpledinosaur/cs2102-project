368937818fd5c108e4884f8877be89dab10029fa
Qi Ji <qjqqyy@gmail.com>
2019-10-29 21:09:39 +0800

    implement registration [2/2]

--- routes/init.js
+++ routes/init.js
@@ -7,18 +7,33 @@ const router = express.Router();
 // SQL Connection
 const { Pool } = require('pg');
 const pool = new Pool({
-    connnectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL
 });
+const salt  = bcrypt.genSaltSync(10);
 
 function initRouter(app) {
     app.use('/', require('./indexRoute'));
     app.use('/about', require('./about'));
 
     app.get('/register', passport.antiMiddleware(), register);
+    app.post('/register', passport.antiMiddleware(), create_user);
 }
 
 function register(req, res, next) {
     res.render('register', { page: 'register', auth: false});
+}
+
+function create_user(req, res, next) {
+    const username = req.body.username;
+    const passwordHash = bcrypt.hashSync(req.body.password, salt);
+    const fullname = req.body.fullname;
+
+    pool.query(sql_query.query.create_user, [username, passwordHash, fullname])
+        .then(() => res.redirect("/login"))
+        .catch(err => {
+            console.log("Error adding user", err);
+            res.redirect("/register?reg=fail");
+        })
 }
 
 module.exports = initRouter;
--- sql/index.js
+++ sql/index.js
@@ -2,7 +2,9 @@ const sql = {}
 
 sql.query = {
     // auth
-    userpass: 'SELECT * FROM Users WHERE uname = $1'
+    userpass: 'SELECT * FROM Users WHERE uname = $1',
+
+    create_user: 'INSERT INTO Users (uname, pwd, fname) VALUES ($1, $2, $3)',
 }
 
 module.exports = sql
